82e61288eab765a390eb02d9804cdc13317d947c
bighenry1024 <13520039732@139.com>
2016-07-13 15:43:24 +0800

    Create build_table.sh

---
+++ build_table.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mysqld_safe &
+#mysqladmin -uroot password $MYSQL_ROOT_PASSWORD
+sleep 10
+mysql -e  "grant all on *.* to '$MYSQL_USER'@'%' identified by  '$MYSQL_PASS' with grant option;"
+mysql -e "create database dbtest;"
+mysql -e "create table dbtest.t_test(id int primary key,name varchar(30) not null) default charset=utf8;"
+mysql -e "insert into dbtest.t_test values (1,'dfll'),(2,'df');"
+#mysqladmin -uroot password $MYSQL_ROOT_PASSWORD
